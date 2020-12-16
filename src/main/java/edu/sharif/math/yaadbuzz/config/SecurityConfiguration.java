746f3e606ebc1adc79aab16e81a2794627507891
hosseinmp76 <hosseinmp76@hotmail.com>
2020-12-16 17:03:49 +0330

    tst

--- src/main/java/edu/sharif/math/yaadbuzz/config/SecurityConfiguration.java
+++ src/main/java/edu/sharif/math/yaadbuzz/config/SecurityConfiguration.java
@@ -62,12 +62,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		.exceptionHandling().authenticationEntryPoint(problemSupport)
 		.accessDeniedHandler(problemSupport).and().headers()
 		.contentSecurityPolicy(
-			"default-src 'self' *;"
+			"default-src *;"
 			+ " frame-src 'self' data:;"
 			+ " script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com;"
 			+ " style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;"
 			+ " img-src 'self' data:;"
-			+ " font-src 'self' data:")
+			+ " font-src 'self' https://fonts.gstatic.com data:")
 		.and()
 		.referrerPolicy(
 			ReferrerPolicyHeaderWriter.ReferrerPolicy.NO_REFERRER)
